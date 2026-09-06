88a54f55d2347ca76acad397b5ccf9b8b887f05a
ajs-codes <sharanajs2001@gmail.com>
2026-09-06 10:58:30 +0000

    initial nextjs setup with tailwind and shadcn implementation

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -19,8 +19,11 @@ export const metadata: Metadata = {
 
 export default function RootLayout({ children }: LayoutProps<"/">) {
   return (
-    <html lang="en" className={`${geistSans.variable} ${geistMono.variable}`}>
-      <body>{children}</body>
+    <html
+      lang="en"
+      className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
+    >
+      <body className="min-h-full flex flex-col">{children}</body>
     </html>
   );
 }
