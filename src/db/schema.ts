348e9163008e0bbc2bdb58c29c3c5e7c99a24c4f
ajs-codes <sharanajs2001@gmail.com>
2026-09-08 07:54:57 +0000

    added user table in db

--- src/db/schema.ts
+++ src/db/schema.ts
@@ -1,5 +1,14 @@
 import { integer, numeric, sqliteTable, text } from "drizzle-orm/sqlite-core";
 
+export const users = sqliteTable("users", {
+  id: integer("id").primaryKey({ autoIncrement: true }),
+  username: text("username").notNull().unique(),
+  email: text("email").notNull().unique(),
+  passwordHash: text("passwordhash").notNull(),
+  createdAt: numeric("created_at").notNull(),
+  updatedAt: numeric("updated_at"),
+});
+
 export const genres = sqliteTable("genres", {
   id: integer("id").primaryKey({ autoIncrement: true }),
   tmdbId: integer("tmdb_id").notNull().unique(),
@@ -9,3 +18,5 @@ export const genres = sqliteTable("genres", {
 
 export type Genre = typeof genres.$inferSelect;
 export type NewGenre = typeof genres.$inferInsert;
+export type User = typeof users.$inferSelect;
+export type NewUser = typeof users.$inferInsert;
