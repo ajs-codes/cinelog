4ef2ba62aea9ae515d843ee6ebe04bf2b4fba203
ajs-codes <sharanajs2001@gmail.com>
2026-09-11 11:52:00 +0530

    load uncached images fix

--- next.config.ts
+++ next.config.ts
@@ -2,15 +2,12 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
   images: {
+    unoptimized: true,
     remotePatterns: [
       {
         hostname: "image.tmdb.org",
         protocol: "https",
-      },
-      {
-        hostname: "www.figma.com",
-        protocol: "https",
-      },
+      }
     ],
   },
 };
